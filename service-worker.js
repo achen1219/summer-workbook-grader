14ad7eb0dadba4bc398eb60006dcba619e7a21b1
achen1219 <91033884+achen1219@users.noreply.github.com>
2026-06-13 23:46:18 -0400

    Add offline cache worker

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "summer-workbook-grader-v1";
+const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
